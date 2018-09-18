4895a705cbca0ed439ca6b41a3253b3aac16845f
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2018-09-17 14:58:21 -1000

    correct stacking/page order

--- main.js
+++ main.js
@@ -11,76 +11,85 @@ var tmpm = new Float32Array(16)
 var tmpv = new Float32Array(3)
 
 var papers = {
-  l0: {
-    positions: [-0.5,-0.5,+0.5,-0.5,+0.5,+0.5,-0.5,+0.5],
-    cells: [[0,1,2],[0,2,3]],
-    pose: new Float32Array(16),
-    model: new Float32Array(16),
-    pivot: [0.0,0.0,0.0],
-    color: [0,0,0]
-  },
-  l1: {
-    parent: 'l0',
-    positions: [+0.5,-0.5,+1.5,-0.5,+1.5,+0.5,+0.5,+0.5],
-    cells: [[0,1,2],[0,2,3]],
-    pose: new Float32Array(16),
-    model: new Float32Array(16),
-    pivot: [0.5,0.5,0.0],
-    color: [0,0,1]
-  },
-  l2: {
-    parent: 'l1',
-    positions: [+1.5,-0.5,+2.5,-0.5,+2.5,+0.5,+1.5,+0.5],
-    cells: [[0,1,2],[0,2,3]],
-    pose: new Float32Array(16),
-    model: new Float32Array(16),
-    pivot: [1.5,0.5,0.0],
-    color: [0,1,0]
-  },
-  l3: {
-    parent: 'l2',
-    positions: [+2.5,-0.5,+3.5,-0.5,+3.5,+0.5,+2.5,+0.5],
-    cells: [[0,1,2],[0,2,3]],
-    pose: new Float32Array(16),
-    model: new Float32Array(16),
-    pivot: [2.5,0.5,0.0],
-    color: [0,1,1]
-  },
-  u0: {
-    parent: 'l0',
-    positions: [-0.5,+0.5,+0.5,+0.5,+0.5,+1.5,-0.5,+1.5],
-    cells: [[0,1,2],[0,2,3]],
-    pose: new Float32Array(16),
-    model: new Float32Array(16),
-    pivot: [+0.0,+0.5,0.0],
-    color: [1,0,0]
-  },
-  u1: {
-    parent: 'u0',
+  p0: {
+    parent: 'p7',
     positions: [+0.5,+0.5,+1.5,+0.5,+1.5,+1.5,+0.5,+1.5],
     cells: [[0,1,2],[0,2,3]],
     pose: new Float32Array(16),
     model: new Float32Array(16),
     pivot: [+0.5,+0.5,0.0],
-    color: [1,0,1]
+    color: [0,0,0],
+    page: 0
   },
-  u2: {
-    parent: 'u1',
+  p1: {
+    parent: 'p0',
     positions: [+1.5,+0.5,+2.5,+0.5,+2.5,+1.5,+1.5,+1.5],
     cells: [[0,1,2],[0,2,3]],
     pose: new Float32Array(16),
     model: new Float32Array(16),
     pivot: [+1.5,+0.5,0.0],
-    color: [1,1,0]
+    color: [0,0,1],
+    page: 1
   },
-  u3: {
-    parent: 'u2',
+  p2: {
+    parent: 'p1',
     positions: [+2.5,+0.5,+3.5,+0.5,+3.5,+1.5,+2.5,+1.5],
     cells: [[0,1,2],[0,2,3]],
     pose: new Float32Array(16),
     model: new Float32Array(16),
     pivot: [+2.5,+0.5,0.0],
-    color: [1,1,1]
+    color: [0,1,0],
+    page: 2
+  },
+  p3: {
+    parent: 'p4',
+    positions: [+2.5,-0.5,+3.5,-0.5,+3.5,+0.5,+2.5,+0.5],
+    cells: [[0,1,2],[0,2,3]],
+    pose: new Float32Array(16),
+    model: new Float32Array(16),
+    pivot: [2.5,0.5,0.0],
+    color: [0,1,1],
+    page: 3
+  },
+  p4: {
+    parent: 'p5',
+    positions: [+1.5,-0.5,+2.5,-0.5,+2.5,+0.5,+1.5,+0.5],
+    cells: [[0,1,2],[0,2,3]],
+    pose: new Float32Array(16),
+    model: new Float32Array(16),
+    pivot: [1.5,0.5,0.0],
+    color: [1,0,0],
+    page: 4
+  },
+  p5: {
+    parent: 'p6',
+    positions: [+0.5,-0.5,+1.5,-0.5,+1.5,+0.5,+0.5,+0.5],
+    cells: [[0,1,2],[0,2,3]],
+    pose: new Float32Array(16),
+    model: new Float32Array(16),
+    pivot: [0.5,0.5,0.0],
+    color: [1,0,1],
+    page: 5
+  },
+  p6: {
+    parent: null,
+    positions: [-0.5,-0.5,+0.5,-0.5,+0.5,+0.5,-0.5,+0.5],
+    cells: [[0,1,2],[0,2,3]],
+    pose: new Float32Array(16),
+    model: new Float32Array(16),
+    pivot: [0.0,0.0,0.0],
+    color: [1,1,0],
+    page: 6
+  },
+  p7: {
+    parent: 'p6',
+    positions: [-0.5,+0.5,+0.5,+0.5,+0.5,+1.5,-0.5,+1.5],
+    cells: [[0,1,2],[0,2,3]],
+    pose: new Float32Array(16),
+    model: new Float32Array(16),
+    pivot: [+0.0,+0.5,0.0],
+    color: [1,1,1],
+    page: 7
   }
 }
 var paperProps = Object.values(papers)
@@ -94,7 +103,7 @@ var ease = require('eases')
 var PI = Math.PI
 var tm = require('./tm.js')({
   page0Flip: {
-    state: { x: PI, y: PI, page: 0, offset: [0,0.35,-1.5], flip: PI },
+    state: { x: PI, y: PI, page: 0, offset: [0,0.35,-1.5], flip: +PI },
     easing: { x: ease.sineOut, y: ease.sineOut }
   },
   page0: {
@@ -110,11 +119,15 @@ var tm = require('./tm.js')({
     easing: { x: ease.sineOut, y: ease.sineOut }
   },
   page3: {
-    state: { x: PI, y: PI, page: 3, offset: [0,0.35,-1.5], flip: 0 },
+    state: { x: PI, y: PI, page: 3, offset: [-0.5,0.35,-1.5], flip: 0 },
     easing: { x: ease.sineOut, y: ease.sineOut }
   },
-  page3Flip: {
-    state: { x: PI, y: PI, page: 3, offset: [0,0.35,-1.5], flip: -PI },
+  page4: {
+    state: { x: PI, y: PI, page: 4, offset: [0,0.35,-1.5], flip: 0 },
+    easing: { x: ease.sineOut, y: ease.sineOut }
+  },
+  page4Flip: {
+    state: { x: PI, y: PI, page: 4, offset: [0,0.35,-1.5], flip: -PI },
     easing: { x: ease.sineOut, y: ease.sineOut }
   },
   fold0: {
@@ -142,15 +155,15 @@ window.addEventListener('wheel', frame)
 
 window.addEventListener('keydown', function (ev) {
   if (ev.key === 'ArrowRight') {
-    if (!state.folded && state.page === 3) {
+    if (!state.folded && state.page === 4) {
       tm.go([
         'fold1',
         speed,
         'fold0',
         speed,
-        'page3',
+        'page4',
         speed,
-        'page3Flip'
+        'page4Flip'
       ])
       state.folded = true
     } else if (!state.folded) {
@@ -161,19 +174,19 @@ window.addEventListener('keydown', function (ev) {
         speed,
         'page' + state.page,
         speed,
-        'page' + String((state.page+1)%4)
+        'page' + String((state.page+1)%5)
       ])
       state.folded = true
-    } else if (state.page === 3) {
-      tm.go([ 'page3', speed, 'page3Flip' ])
+    } else if (state.page === 4) {
+      tm.go([ 'page4', speed, 'page4Flip' ])
     } else {
       tm.go([
         'page' + state.page,
         speed,
-        'page' + String((state.page+1)%4)
+        'page' + String((state.page+1)%5)
       ])
     }
-    state.page = (state.page+1)%4
+    state.page = (state.page+1)%5
     frame()
   } else if (ev.key === 'ArrowLeft') {
     if (!state.folded && state.page === 0) {
@@ -195,7 +208,7 @@ window.addEventListener('keydown', function (ev) {
         speed,
         'page' + state.page,
         speed,
-        'page' + String((state.page+3)%4)
+        'page' + String((state.page+4)%5)
       ])
       state.folded = true
     } else if (state.page === 0) {
@@ -204,10 +217,10 @@ window.addEventListener('keydown', function (ev) {
       tm.go([
         'page' + state.page,
         speed,
-        'page' + String((state.page+3)%4)
+        'page' + String((state.page+4)%5)
       ])
     }
-    state.page = (state.page+3)%4
+    state.page = (state.page+4)%5
     frame()
   } else if (ev.key === ' ' && state.folded) {
     tm.go([ 'page' + state.page, 0.5, 'fold0', 0.5, 'fold1' ])
@@ -240,47 +253,78 @@ function update (t) {
     mat4.identity(paper.pose)
   })
   var { x, y, offset, page, flip } = tm.tick(t)
-  var fudge = 0.01
   paperProps.forEach(function (paper) {
     vec3.copy(paper.offset, offset)
+    paper.offset[2] = paper.page * x * 0.004
+      * (paper.page < page*2 ? -1 : +1)
+      * (Math.abs(flip) > PI*0.5 ? -1 : +1)
   })
 
-  mat4.rotateY(pose.l1,pose.l1,x*(0.5-fudge))
-  mat4.rotateY(pose.l2,pose.l2,-x*(1-fudge))
-  mat4.rotateY(pose.l3,pose.l3,x*(0.5-fudge))
-  mat4.rotateY(pose.u1,pose.u1,x*(0.5-fudge))
-  mat4.rotateY(pose.u2,pose.u2,-x*(1-fudge))
-  mat4.rotateY(pose.u3,pose.u3,x*(0.5-fudge))
-  mat4.rotateX(pose.u0,pose.u0,x*(1-fudge*2))
-  mat4.rotateX(pose.l0,pose.l0,-x*fudge)
+  mat4.rotateY(pose.p5,pose.p5,+x*0.5)
+  mat4.rotateY(pose.p4,pose.p4,-x*1.0)
+  mat4.rotateY(pose.p3,pose.p3,+x*0.5)
+  mat4.rotateY(pose.p0,pose.p0,+x*0.5)
+  mat4.rotateY(pose.p1,pose.p1,-x*1.0)
+  mat4.rotateY(pose.p2,pose.p2,+x*0.5)
+  mat4.rotateX(pose.p7,pose.p7,+x*1.0)
 
-  mat4.rotateY(pose.l1,pose.l1,+y*(0.5-fudge))
-  mat4.rotateY(pose.l3,pose.l3,+y*(0.5-fudge))
-  mat4.rotateY(pose.u1,pose.u1,-y*(1.5-fudge))
-  mat4.rotateY(pose.u3,pose.u3,+y*(0.5-fudge))
+  mat4.rotateY(pose.p5,pose.p5,+y*0.5)
+  mat4.rotateY(pose.p3,pose.p3,+y*0.5)
+  mat4.rotateY(pose.p0,pose.p0,-y*1.5)
+  mat4.rotateY(pose.p2,pose.p2,+y*0.5)
 
-  mat4.rotateY(pose.l3,pose.l3,
-    -page*(PI-fudge)
-    +Math.max(0,page-1)*(PI-fudge)*2
-    -Math.max(0,page-2)*(PI-fudge)
+  mat4.rotateY(pose.p0,pose.p0,
+    +smstep(0,1,page)*PI
+    -smstep(3,4,page)*PI
   )
-  mat4.rotateY(pose.u3,pose.u3,
-    +page*(PI-fudge)
-    -Math.max(0,page-1)*(PI-fudge)*2
-    +Math.max(0,page-2)*(PI-fudge)
+  mat4.rotateY(pose.p2,pose.p2,
+    +smstep(1,2,page)*PI
+    -smstep(0,1,page)*PI
   )
-  mat4.rotateY(pose.l1,pose.l1,
-    -Math.max(0,page-1)*(PI-fudge)
-    +Math.max(0,page-2)*(PI-fudge)*2
+  mat4.rotateY(pose.p3,pose.p3,
+    -smstep(1,2,page)*PI
+    +smstep(2,3,page)*PI
   )
-  mat4.rotateY(pose.u1,pose.u1,
-    +Math.max(0,page-1)*(PI-fudge)
-    -Math.max(0,page-2)*(PI-fudge)*2
+  mat4.rotateY(pose.p5,pose.p5,
+    -smstep(2,3,page)*PI
+    +smstep(3,4,page)*PI
   )
-  mat4.rotateY(pose.l0,pose.l0,
-    -Math.max(0,page-2)*(PI-fudge)
+  mat4.rotateY(pose.p6,pose.p6,
+    -smstep(3,4,page)*PI
     + flip
   )
+
+  /*
+  mat4.rotateY(pose.p3,pose.p3,
+    -page*PI
+    +Math.max(0,page-1)*PI*2
+    -Math.max(0,page-2)*PI
+  )
+  mat4.rotateY(pose.p2,pose.p2,
+    +page*PI
+    -Math.max(0,page-1)*PI*2
+    +Math.max(0,page-2)*PI
+  )
+  mat4.rotateY(pose.p5,pose.p5,
+    -Math.max(0,page-1)*PI
+    +Math.max(0,page-2)*PI*2
+  )
+  mat4.rotateY(pose.p0,pose.p0,
+    +Math.max(0,page-1)*PI
+    -Math.max(0,page-2)*PI*2
+  )
+  mat4.rotateY(pose.p6,pose.p6,
+    -Math.max(0,page-2)*PI
+    + flip
+  )
+  */
+}
+
+function smstep (a, b, x) {
+  return clamp(0, 1, (x-a) / (b-a))
+}
+function clamp (a, b, x) {
+  return Math.max(a, Math.min(b, x))
 }
 
 function updateModels () {
@@ -307,7 +351,7 @@ function paper (regl) {
       varying vec3 vpos;
       void main () {
         vec3 N = normalize(cross(dFdx(vpos),dFdy(vpos)));
-        gl_FragColor = vec4(color + (0.5+N*0.5)*0.2,1);
+        gl_FragColor = vec4(color*0.5+vec3(0.5),1);
       }
     `,
     vert: `
